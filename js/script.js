621cfd5f7baa3ffc161130efb861a4020ef76f4d
LeilaITalbot <leilaitalbot@gmail.com>
2020-02-19 02:26:45 +0000

    project work

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let picture=$(".pic");
+let songName=$(".title");
+let singer=$(".artist");
+let url=$(".link");
+let seconds=$(".time");
+ 
+picture=[""];
+songName=["Next Stage!!","Time Capsule","JUMP AND FLY"];
+singer=["SutoPuri", "Riinu", "Jel"];
+url=["https://open.spotify.com/track/4CTuceahOjQhaEYFtCIciE", "https://open.spotify.com/track/2CKHotRVsdrlWYn17h5lRk","https://open.spotify.com/track/6FwocjvOrDHGQ6ruRyDReD"];
+seconds=[11, 12, 14];
+
+playlist=[picture, songName, singer, url, seconds];
+console.log(playlist);
